b7c656dcbfd91089dc6f6d29e088f7549f897786
Harold <harold@mrtz.fr>
2025-06-26 08:48:46 +0200

    Check that pre-commit passes without error on all files

--- src/clock.c
+++ src/clock.c
@@ -2,11 +2,10 @@
 
 #include "clock.h"
 
-void clock_setup(void)
-{
-	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE16_72MHZ]);
-	rcc_periph_clock_enable(RCC_AFIO);
-	rcc_periph_clock_enable(RCC_GPIOA);
-	rcc_periph_clock_enable(RCC_GPIOB);
-	rcc_periph_clock_enable(RCC_USART1);
+void clock_setup(void) {
+  rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE16_72MHZ]);
+  rcc_periph_clock_enable(RCC_AFIO);
+  rcc_periph_clock_enable(RCC_GPIOA);
+  rcc_periph_clock_enable(RCC_GPIOB);
+  rcc_periph_clock_enable(RCC_USART1);
 }
